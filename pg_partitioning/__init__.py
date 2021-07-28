d1b331e7064050dee505e3c40d60f3a0df8cb890
Fuzzwah <fuzzwah@github.com>
2021-07-28 15:15:47 +1000

    support django 3.1

--- pg_partitioning/__init__.py
+++ pg_partitioning/__init__.py
@@ -1,7 +1,7 @@
 """
 A Django extension that supports PostgreSQL 11 time ranges and list partitioning.
 """
-REQUIRED_DJANGO_VERSION = [(2, 0), (3, 0)]
+REQUIRED_DJANGO_VERSION = [(2, 0), (3, 0), (3, 1)]
 DJANGO_VERSION_ERROR = "django-pg-partitioning isn't available on the currently installed version of Django."
 
 try:
@@ -9,10 +9,10 @@ try:
 except ImportError:
     raise ImportError(DJANGO_VERSION_ERROR)
 
-if REQUIRED_DJANGO_VERSION[0] > tuple(django.VERSION[:2]) or tuple(django.VERSION[:2]) > REQUIRED_DJANGO_VERSION[1]:
+if REQUIRED_DJANGO_VERSION[0] > tuple(django.VERSION[:2]) or tuple(django.VERSION[:2]) > REQUIRED_DJANGO_VERSION[-1]:
     raise ImportError(DJANGO_VERSION_ERROR)
 
 
-__version__ = "0.11"
+__version__ = "0.12"
 
 default_app_config = "pg_partitioning.apps.AppConfig"
